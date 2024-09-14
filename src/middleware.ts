4d1bf9f66616f381130a60857af30f73bda098cf
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2024-09-14 18:23:59 +0900

    feat: added authentication

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import type { NextRequest } from "next/server";
+
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default function middleware(request: NextRequest) {
+  return withAuth(request);
+}
+
+export const config = {
+  matcher: ["/app"],
+};
